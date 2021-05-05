e1885da071f6e7a38f9b6db0a728aad8c6d5e286
Atmanand <sanketgauns8@gmail.com>
2021-05-05 11:40:15 +0530

    added first python file

--- first-python.py
+++ first-python.py
@@ -1,1 +1,2 @@
-print("HELLO GITHUB\nThis is my first program in python to be uploaded on github.")
+print("HELLO GITHUB\nThis is my first program in python to be uploaded on github.");
+#just edited to make a commit for specifically python
