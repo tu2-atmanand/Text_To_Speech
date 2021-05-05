22cc9997afbdaf3f9c44460e158cc094d905de47
Atmanand <sanketgauns8@gmail.com>
2021-05-05 11:36:17 +0530

    added c++ file

---
+++ first-c++.cpp
@@ -0,0 +1,7 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    cout<<"HELLO GITHUB\nThis is my first program to be uploded on github\n";
+    return 0;
+}
---
+++ first-python.py
@@ -0,0 +1,1 @@
+print("HELLO GITHUB\nThis is my first program in python to be uploaded on github.")
