979dae596e613e8c0685883a58daa1ca7c438c2b
Atmanand <sanketgauns8@gmail.com>
2021-05-30 19:11:40 +0530

    added first bash shell file

---
+++ first-shell.sh
@@ -0,0 +1,4 @@
+echo -e "Hello Github\nLets begin"
+espeak "Hello Github! Lets begin"
+
+echo "if cant able to hear\n install espeak : for ubuntu-based : 'apt-get install espeak'\nfor alpine-bsesd : 'apk add espeak'\nfor fedora-based : 'dnf install espeak'\nfor arch-based :  'pacman install espeak' "
