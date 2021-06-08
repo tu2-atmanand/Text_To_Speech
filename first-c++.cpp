2e29096b6034a7813b0a0be523988452692db69c
Atmanand <sanketgauns8@gmail.com>
2021-06-08 18:39:28 +0530

    add TTS functionality to c and cpp file

--- first-c++.cpp
+++ first-c++.cpp
@@ -1,7 +1,8 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 int main()
 {
-    cout<<"HELLO GITHUB\nThis is my first program to be uploded on github\n";
+    cout << "HELLO GITHUB\nThis is my first program to be uploded on github\n";
+    system("speak.vbs \"Hello GitHub , Lets programming begins\"");
     return 0;
 }
--- first-c.c
+++ first-c.c
@@ -1,6 +1,8 @@
-#include<stdio.h>
+#include <stdio.h>
+// #include <iostream>
 int main()
 {
     printf("Hello Github\nThis is my first c program to be uploaded in github.");
+    system("speak.vbs \"Hello GitHub , lets start C programming\"");
     return 0;
 }
