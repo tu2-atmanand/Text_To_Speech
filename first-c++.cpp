694912c97ffea2a5157bfd0617dd9d55b2ed8038
Atmanand <sanketgauns8@gmail.com>
2021-06-09 11:16:50 +0530

    add speech functionality to python file and add comment in cpp file

--- first-c++.cpp
+++ first-c++.cpp
@@ -3,6 +3,7 @@ using namespace std;
 int main()
 {
     cout << "HELLO GITHUB\nThis is my first program to be uploded on github\n";
+    // this is using the help of visual Basics which uses sapi. But there is a direct way to do it using visual studio like ide and creating a project to add those required header files which becomes slightly coplicated in direct way of writing cpp programs.
     system("speak.vbs \"Hello GitHub , Lets programming begins\"");
     return 0;
 }
--- first-python.py
+++ first-python.py
@@ -1,2 +1,10 @@
-print("HELLO GITHUB\nThis is my first program in python to be uploaded on github.");
-#just edited to make a commit for specifically python
+import pyttsx3
+
+# print("HELLO GITHUB\nThis is my first program in python to be uploaded on github.")
+engine = pyttsx3.init('sapi5')
+voices = engine.getProperty('voices')
+# print(voices[1].id)
+engine.setProperty('voice', voices[1].id)
+engine.say("Hello GitHub, lets start programming in python")
+engine.runAndWait()
+# just edited to make a commit for specifically python
